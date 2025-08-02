d9c2bb371b45df0f88105d629b8a2f29ab7fe3ed
elly-po <ellypaul400@gmail.com>
2025-08-02 11:20:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { decodePumpfun } from './pumpfunDecoder.js';
+/*import { decodePumpfun } from './pumpfunDecoder.js';
 
 const signature = '5g6xXeyvBk526k1tvZENcrsJJ6HffgyunfgTcouixmHYQtZzgaSXAGVeoSiENVHMJPUSBxCftruwsZiwozbmsQfd';
 
@@ -8,4 +8,16 @@ decodePumpfun(signature)
   })
   .catch(err => {
     console.error('Error:', err.message);
+  });*/
+
+import { decodeRaydium } from './raydiumDecoder.js';
+
+const signature = 'INSERT_RAYDIUM_TX_SIGNATURE_HERE';
+
+decodeRaydium(signature)
+  .then(data => {
+    console.log('Decoded Raydium Data:', data);
+  })
+  .catch(err => {
+    console.error('Raydium Decode Error:', err.message);
   });
